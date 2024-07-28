205702bd04ec19c89fb9a5f0486a7cda2cc6de7b
hozier_dai <hozierdai@gmail.com>
2024-07-28 15:29:53 -0400

    add utls.py

--- fine-tuning/check-data-format/check_dataset_format_for_3.5.py
+++ fine-tuning/check-data-format/check_dataset_format_for_3.5.py
@@ -10,7 +10,6 @@ from check_dataset_format import CheckDatasetFormat
 class CheckDatasetFormatFor3P5(CheckDatasetFormat):
     def __init__(self, filepath="../dataset/prompt_3.5_train.jsonl"):
         super().__init__(file_path=filepath)
-        self.encoding = tiktoken.get_encoding("cl100k_base")
 
     def error_check(self):
         """
@@ -55,30 +54,6 @@ class CheckDatasetFormatFor3P5(CheckDatasetFormat):
                 print(f"{k}: {v}")
         else:
             print("No errors found")
-
-    def num_tokens_from_messages(self, messages, tokens_per_message=3, tokens_per_name=1):
-        num_tokens = 0
-        for message in messages:
-            num_tokens += tokens_per_message
-            for key, value in message.items():
-                num_tokens += len(self.encoding.encode(value))
-                if key == "name":
-                    num_tokens += tokens_per_name
-        num_tokens += 3
-        return num_tokens
-
-    def num_assistant_tokens_from_messages(self, messages):
-        num_tokens = 0
-        for message in messages:
-            if message["role"] == "assistant":
-                num_tokens += len(self.encoding.encode(message["content"]))
-        return num_tokens
-
-    def print_distribution(self, values, name):
-        print(f"\n#### Distribution of {name}:")
-        print(f"min / max: {min(values)}, {max(values)}")
-        print(f"mean / median: {np.mean(values)}, {np.median(values)}")
-        print(f"p5 / p95: {np.quantile(values, 0.1)}, {np.quantile(values, 0.9)}")
 
 
 if __name__ == "__main__":
